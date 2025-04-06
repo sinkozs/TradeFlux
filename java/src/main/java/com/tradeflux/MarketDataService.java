1eaa186070f47db6b560ba5d90eb41e77c8f1ea3
Zsofia Sinko <zsofia.sinko@outlook.com>
2025-04-06 19:06:01 +0200

    Implement getHistoricalOHLCDataAPI and Builders

--- java/src/main/java/com/tradeflux/MarketDataService.java
+++ java/src/main/java/com/tradeflux/MarketDataService.java
@@ -1,13 +1,40 @@
 package com.tradeflux;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import okhttp3.WebSocket;
 import java.util.*;
 
 public class MarketDataService {
     private BinanceConnector connector = new BinanceConnector();
+
+    private Optional getSymbolsRequestParams(List<String> symbols) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        HashMap<String, String> params = new HashMap<>();
+
+        params.put("symbols", mapper.writeValueAsString(symbols));
+        return Optional.of(params);
+    }
+
+    private List<Map<String, Object>> parseCoinPricesFromJson(JsonNode apiResponse) {
+        List<Map<String, Object>> response = new ArrayList<>();
+        for (JsonNode symbolNode : apiResponse) {
+            Map<String, Object> coinInfo = new HashMap<>();
+            if (symbolNode.has("symbol")) {
+                String symbol = symbolNode.get("symbol").asText();
+                coinInfo.put("symbol", symbol);
+            }
+            if (symbolNode.has("price")) {
+                Double price = symbolNode.get("price").asDouble();
+                coinInfo.put("price", price);
+            }
+            response.add(coinInfo);
+        }
+        return response;
+    }
 
     public List<Map<String, Object>> getAvailableCoinsAPI() throws IOException {
         /**
@@ -56,27 +83,121 @@ public class MarketDataService {
         return response;
     }
 
-    public List<Map<String, Object>> getCurrentPriceAPI(List<String> symbols) throws IOException {
-        List<Map<String, Object>> response = new ArrayList<>();
-        ObjectMapper mapper = new ObjectMapper();
-        HashMap<String, String> params = new HashMap<>();
-
-        params.put("symbols", mapper.writeValueAsString(symbols));
-
+    public List<Map<String, Object>> getCurrentPriceAPI(CoinRequestBuilder requestBuilder) throws IOException {
+        HashMap<String, String> params = requestBuilder.buildParams();
         JsonNode apiResponse = connector.makeRESTApiRequest("/ticker/price", Optional.of(params));
 
-        for (JsonNode symbolNode : apiResponse) {
-            Map<String, Object> coinInfo = new HashMap<>();
-            if (symbolNode.has("symbol")) {
-                String symbol = symbolNode.get("symbol").asText();
-                coinInfo.put("symbol", symbol);
+        return parseCoinPricesFromJson(apiResponse);
+    }
+
+    public List<Map<String, Object>> getHistoricalOHLCDataAPI(OHLCRequestBuilder requestBuilder) throws IOException {
+        List<Map<String, Object>> response = new ArrayList<>();
+        HashMap<String, String> params = requestBuilder.buildParams();
+
+        JsonNode apiResponse = connector.makeRESTApiRequest("/klines", Optional.of(params));
+
+        for (JsonNode candleNode : apiResponse) {
+            if (candleNode.isArray()) {
+                Map<String, Object> ohlcData = new HashMap<>();
+
+                ohlcData.put("klineOpenTime", candleNode.get(0).asLong());
+                ohlcData.put("openPrice", candleNode.get(1).asText());
+                ohlcData.put("highPrice", candleNode.get(2).asText());
+                ohlcData.put("lowPrice", candleNode.get(3).asText());
+                ohlcData.put("closePrice", candleNode.get(4).asText());
+                ohlcData.put("volume", candleNode.get(5).asText());
+                ohlcData.put("klineCloseTime", candleNode.get(6).asLong());
+                ohlcData.put("quoteAssetVolume", candleNode.get(7).asText());
+                ohlcData.put("numberOfTrades", candleNode.get(8).asInt());
+                ohlcData.put("takerBuyBaseAssetVolume", candleNode.get(9).asText());
+                ohlcData.put("takerBuyQuoteAssetVolume", candleNode.get(10).asText());
+
+                response.add(ohlcData);
             }
-            if (symbolNode.has("price")) {
-                Double price = symbolNode.get("price").asDouble();
-                coinInfo.put("price", price);
-            }
-            response.add(coinInfo);
         }
         return response;
+    }
+
+//    public List<Map<String, Object>> streamPriceUpdatesWS(String symbol) throws IOException {
+//        WebSocket ws = connector.connectToWSStream(symbol, "bookTicker");
+//
+//    }
+
+
+    ///  Builders
+    public static class CoinRequestBuilder {
+        private final List<String> symbols;
+
+        public CoinRequestBuilder(List<String> symbols) {
+            this.symbols = symbols;
+        }
+
+        public HashMap<String, String> buildParams() throws JsonProcessingException {
+            ObjectMapper mapper = new ObjectMapper();
+            HashMap<String, String> params = new HashMap<>();
+
+            params.put("symbols", mapper.writeValueAsString(symbols));
+            return params;
+        }
+
+    }
+
+    public static class OHLCRequestBuilder {
+        private final String symbol;
+        private final String interval;
+
+        private Integer limit = 100;
+        private Integer timezone = null;
+        private Long startTime = null;
+        private Long endTime = null;
+
+        public OHLCRequestBuilder(String symbol, String interval) {
+            this.symbol = symbol;
+            this.interval = interval;
+        }
+
+        public OHLCRequestBuilder limit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public OHLCRequestBuilder timezone(Integer timezone) {
+            this.timezone = timezone;
+            return this;
+        }
+
+        public OHLCRequestBuilder startTime(Long startTime) {
+            this.startTime = startTime;
+            return this;
+        }
+
+        public OHLCRequestBuilder endTime(Long endTime) {
+            this.endTime = endTime;
+            return this;
+        }
+
+        public HashMap<String, String> buildParams() {
+            HashMap<String, String> params = new HashMap<>();
+            params.put("symbol", symbol);
+            params.put("interval", interval);
+
+            if (limit != null) {
+                params.put("limit", String.valueOf(limit));
+            }
+
+            if (timezone != null) {
+                params.put("timeZone", String.valueOf(timezone));
+            }
+
+            if (startTime != null) {
+                params.put("startTime", String.valueOf(startTime));
+            }
+
+            if (endTime != null) {
+                params.put("endTime", String.valueOf(endTime));
+            }
+
+            return params;
+        }
     }
 }
